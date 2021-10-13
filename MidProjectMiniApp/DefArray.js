e74d6d05fc6f3e875c5cbc2398785d1b459aa9d1
WanganiaTesalonika <69896956+WanganiaTesalonika@users.noreply.github.com>
2021-10-13 14:04:16 +0800

    Add files via upload

---
+++ MidProjectMiniApp/DefArray.js
@@ -0,0 +1,27 @@
+
+var studentsData = [
+	{
+		id: 1,
+		NIM: 105011910043,
+		fullName: 'Inka Wangania',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Sistem Informasi',
+	},
+	{
+		id: 2,
+		NIM: 103298468867,
+		fullName: 'Rebecca Lie',
+		gender: 'Male',
+		fakultas: 'FEB',
+		study: 'Manajemen',
+	},
+	{
+		id: 3,
+		NIM: 105011932453,
+		fullName: 'Pearly Mole',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Informatika',
+	}
+]
